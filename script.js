261b9eb80c71d165aa7019e09dc0a2ec17b4ae9a
Stas2664 <alinastabceva70@gmail.com>
2025-08-30 13:34:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -395,3 +395,13 @@ function initializeAnimations() {
     });
   });
 })();
+
+// --- iOS Safari 100vh fix ---
+(function() {
+  const setVh = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+  setVh();
+  window.addEventListener('resize', setVh);
+})();
